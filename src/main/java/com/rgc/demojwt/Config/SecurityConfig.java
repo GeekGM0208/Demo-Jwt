315b637567e1a18400b8965396afe148f7fd2425
GeekGM0208 <ricardo.garcia.cuellar@gmail.com>
2024-02-01 23:06:15 -0600

    add configSecurity and AuthenticationFilter

--- src/main/java/com/rgc/demojwt/Config/SecurityConfig.java
+++ src/main/java/com/rgc/demojwt/Config/SecurityConfig.java
@@ -1,14 +1,32 @@
 package com.rgc.demojwt.Config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)//desabilita el CSRF
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .build();
+    }
 
 
 }
---
+++ src/main/java/com/rgc/demojwt/Jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.rgc.demojwt.Jwt;
+
+public class JwtAuthenticationFilter {
+}
